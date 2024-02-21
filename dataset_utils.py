e3b43e8abaebc9780228eb23d328a4db054ff5dc
atadria <tokarska.adrianna@gmail.com>
2024-02-21 19:48:10 +0100

    add pubmed data

--- dataset_utils.py
+++ dataset_utils.py
@@ -13,13 +13,17 @@ DATASETS = [
         'id': 'bigbio/distemist',
         'label': ['passages', 'text']
     },
+    {
+        'id': './pubmed/pubmed.py',
+        'label': ['MedlineCitation', 'Article', ['ArticleTitle', 'Abstract']]
+    }
 ]
 
 
 def get_text(data_item, label):
     if type(label) == str:
         return {'text': data_item[label]}
-    else:
+    elif type(label) == list and len(label) == 2:
         texts = []
         for d in data_item[label[0]]:
             text_ = d[label[1]]
@@ -29,6 +33,18 @@ def get_text(data_item, label):
             elif type(text_) == list:
                 texts.append('\n'.join(text_))
         return {'text': '\n'.join(texts)}
+    else:
+        # pubmed case
+        texts = []
+        d = data_item['MedlineCitation']['Article']
+        title = d['ArticleTitle']
+        abstract = d['Abstract']
+        if abstract:
+            if type(abstract) == str:
+                texts.append(abstract)
+            elif type(abstract) == list:
+                texts.append('\n'.join(abstract))
+            return {'text': '\n'.join(texts)}
 
 
 def get_dataset():
@@ -40,7 +56,9 @@ def get_dataset():
                             trust_remote_code=True)
         lbl = dataset['label']
         for dp in data:
-            multi_data.append(get_text(data_item=dp,
-                                       label=lbl))
+            dp_text = get_text(data_item=dp,
+                               label=lbl)
+            if dp_text:
+                multi_data.append(dp_text)
 
     return Dataset.from_list(multi_data).shuffle()
